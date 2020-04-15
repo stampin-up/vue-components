6bddd1db5923aa2489ba34babe2871a8b1b07b5a
Randy Babcock <rbabcock@stampinup.com>
2020-04-15 14:12:40 -0600

    improvement(styling): directional Callout Arrows
    Directions of arrows are dependent on location of screens

--- src/components/STooltip.vue
+++ src/components/STooltip.vue
@@ -1,33 +1,38 @@
 <template>
   <VTooltip
+    v-scroll="onScroll"
+    v-resize="onResize"
     v-bind="$attrs"
     dark
     open-on-click
+    :top="!bottomAligned"
+    :bottom="bottomAligned"
     :open-on-hover="false"
     :content-class="tooltipStyle"
-    bottom
     max-width="320px"
     open-delay="500"
-    nudge-left="10px"
-    nudge-bottom="8px"
     class="pa-3"
   >
     <template v-slot:activator="{ on }">
       <SBtn
-        v-show="icon"
+        v-show="!text"
         :height="size"
         :width="size"
         icon
         v-on="on"
+        ref="tooltipBtn"
+        class="icon-btn"
       >
         <VIcon :color="color" :size="size">
           {{ icons.mdiHelpCircleOutline }}
         </VIcon>
       </SBtn>
       <SBtn
-        v-show="!icon"
+        v-show="text"
         link-medium
         v-on="on"
+        ref="tooltipLink"
+        class="link-btn"
       >
         {{ text }}
       </SBtn>
@@ -46,8 +51,10 @@ import SBtn from '../components/SBtn.vue'
 })
 export default class TooltipComponent extends Vue {
   icons = { mdiHelpCircleOutline }
-  @Prop({ type: Boolean, default: false })
-  icon!: boolean
+  $refs!:  {
+    tooltipBtn: SBtn,
+    tooltipLink: SBtn 
+  }
 
   @Prop({ type: String, default: null })
   text!: string | null
@@ -61,14 +68,58 @@ export default class TooltipComponent extends Vue {
   @Prop({ type: Boolean, default: false })
   callout!: boolean
 
-  @Prop({ type: Boolean, default: true })
-  bottom!: boolean
+  offsetTopButton = 0
+  offsetLeftButton = 0
+  halfWindowHeight = 0
+  leftEdgeConstraint = 0
+  rightEdgeConstraint = 0
+  bottomAligned = false
+  classList = ''
+
+  setViewableWindowSize () {
+    this.halfWindowHeight = window.innerHeight / 2
+    this.leftEdgeConstraint = 160
+    this.rightEdgeConstraint = window.innerWidth - 160
+  }
+
+  async setTooltipOffset () {
+    const positionY = window.scrollY
+    const button = this.$refs.tooltipBtn.$el as HTMLElement
+    const link = this.$refs.tooltipLink.$el as HTMLElement
+    const offset = this.text ? link.offsetTop : button.offsetTop
+    await this.$nextTick() 
+
+    this.offsetTopButton = offset - window.scrollY
+    this.offsetLeftButton = this.text ? link.offsetLeft: button.offsetLeft
+    this.bottomAligned = this.offsetTopButton <= this.halfWindowHeight
+  } 
+
+  async onResize () {
+    this.setViewableWindowSize()
+    await this.setTooltipOffset()
+  }
+
+  async onScroll () {
+    await this.setTooltipOffset()
+  }
 
   get tooltipStyle () {
-    if (this.callout) {
-      return this.bottom ? 'callout bottom' : 'callout'
+    this.classList = this.callout ? 'callout' : 'tooltip'
+    this.classList = this.offsetTopButton <= this.halfWindowHeight ? `${this.classList} bottom` : `${this.classList} top`
+
+    if (this.offsetLeftButton <= this.leftEdgeConstraint) {
+      this.classList = `${this.classList} left`
+    } else if (this.offsetLeftButton >= this.rightEdgeConstraint) {
+      this.classList = `${this.classList} right`
+    } else {
+      this.classList = `${this.classList} middle`
     }
-    return 'tooltip'
+    return this.classList
+  }
+
+  async mounted () {
+    this.setViewableWindowSize()
+    await this.setTooltipOffset()
   }
 }
 </script>
@@ -81,6 +132,8 @@ export default class TooltipComponent extends Vue {
     opacity: 1 !important;
     box-shadow: rgba(0, 0, 0, 0.3) 4px 4px 4px ;
     border: 1px solid var(--v-border-base);
+    &.top { margin-top: -8px; }
+    &.bottom { margin-top: 8px; }
   }
   .callout:after {
     height: 20px;
@@ -88,18 +141,22 @@ export default class TooltipComponent extends Vue {
     content: "";
     background-color: white;
     position: absolute;
+    transform: rotate(-45deg);
     -moz-transform: rotate(-45deg);
     -webkit-transform: rotate(-45deg);
   }
-  .left:after {
-    left: -11px;
-    bottom: 40%;
+
+  .left:after { left: 16px; }
+  .middle:after { left: 47%; }
+  .right:after { right: 16px; }
+
+  .top:after {
+    bottom: -10px;
     border-left: 1px solid var(--v-border-base);
-    border-top: 1px solid var(--v-border-base);
+    border-bottom: 1px solid var(--v-border-base);
   }
   .bottom:after {
     top: -10px;
-    left: 50%;
     border-right: 1px solid var(--v-border-base);
     border-top: 1px solid var(--v-border-base);
   }
