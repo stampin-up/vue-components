7f7660e3e095dd48b42ca1eb695385fe89c16db2
Dmitry Molotkov <aldarund@gmail.com>
2020-04-28 17:43:34 +0300

    fix: simage with fix for image intesection in vuetify vdialogs (#23)

--- src/components/SImage.vue
+++ src/components/SImage.vue
@@ -1,8 +1,20 @@
 <template>
-  <VLazyImage :src-placeholder="placeholder" v-bind="$attrs" class="lazy-image" />
+  <img
+    v-if="src"
+    :src="srcImage"
+    :srcset="srcsetImage"
+    :src-placeholder="placeholder"
+    v-bind="$attrs"
+    class="lazy-image"
+    :class="{
+      'v-lazy-image-loaded': loaded
+    }"
+    @load="load"
+  >
 </template>
 
 <script lang="ts">
+// adapted from https://github.com/alexjoverm/v-lazy-image/blob/master/src/index.js
 import { Component, Vue, Prop } from 'nuxt-property-decorator'
 import VLazyImage from 'v-lazy-image'
 
@@ -14,6 +26,56 @@ import VLazyImage from 'v-lazy-image'
 })
 export default class SBtn extends Vue {
   @Prop({ required: false, default: 'https://via.placeholder.com/360', type: String }) placeholder!: string
+  @Prop({ required: false, type: String }) src?: string
+  @Prop({ required: false, type: String }) srcset?: string
+
+  observer: IntersectionObserver | null = null
+  intersected = false
+  loaded = false
+  intersectionOptions = {}
+
+  get srcImage () {
+    return this.intersected && this.src ? this.src : this.placeholder
+  }
+
+  get srcsetImage () {
+    return this.intersected && this.srcset ? this.srcset : false
+  }
+
+  get intersectionAvailable () {
+    return typeof window !== 'undefined' && 'IntersectionObserver' in window
+  }
+
+  load () {
+    if (this.$el.getAttribute('src') !== this.placeholder) {
+      this.loaded = true
+      this.$emit('load')
+    }
+  }
+
+  mounted () {
+    if (this.intersectionAvailable) {
+      this.observer = new IntersectionObserver((entries) => {
+        const [image] = entries.slice(-1)
+        if (image.isIntersecting) {
+          this.intersected = true
+          this.cleanup()
+          this.$emit('intersect')
+        }
+      }, this.intersectionOptions)
+      this.observer.observe(this.$el)
+    }
+  }
+
+  cleanup () {
+    if (this.observer) {
+      this.observer.disconnect()
+    }
+  }
+
+  destroyed () {
+    this.cleanup()
+  }
 }
 </script>
 
@@ -23,5 +85,4 @@ export default class SBtn extends Vue {
   width: auto;
   max-width: 100%;
 }
-
 </style>
