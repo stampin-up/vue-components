59f47c495d69e9e3f974fd52124d08bf65623131
kglazier <48601239+kglazier@users.noreply.github.com>
2020-06-16 09:37:06 -0600

    feat(dialog component): added component (#38)
    * feat(dialog component): added component
    
    * feat(sdialog): add activator slot then fix padding
    
    * feat(sdialog): remove unused padding

---
+++ src/components/SDialog.vue
@@ -0,0 +1,107 @@
+<template>
+  <VDialog
+    v-model="innerValue"
+    v-bind="$attrs"
+    :width="width"
+    scrollable
+    @click:outside="close"
+  >
+    <template v-slot:activator="slotData">
+      <slot name="activator" v-bind="slotData" />
+    </template>
+    <VCard class="text-center pa-2" max-height="700" data-testid="confirm-dialog">
+      <VRow no-gutters>
+        <VCol cols="12" class="text-right">
+          <SBtn
+            color="primary"
+            text
+            @click="close"
+          >
+            <VIcon>{{ icons.mdiClose }}</VIcon>
+          </SBtn>
+        </VCol>
+      </VRow>
+      <VCardTitle
+        v-if="title"
+        class="text-uppercase font-weight-black pa-0"
+      >
+        <VRow no-gutters>
+          <VCol cols="12" class="px-4 py-2">
+            <h4>
+              {{ title }}
+            </h4>
+          </VCol>
+        </VRow>
+      </VCardTitle>
+      <VCardText
+        v-if="text"
+        class="text--primary font14 py-2"
+      >
+        {{ text }}
+      </VCardText>
+      <slot />
+      <VCardActions v-if="cancelText">
+        <VRow no-gutters>
+          <VCol cols="12">
+            <SBtn
+              class="mb-3"
+              link-small
+              data-testid="confirm-dialog-btn-cancel"
+              @click="close"
+            >
+              {{ cancelText }}
+            </SBtn>
+          </VCol>
+        </VRow>
+      </VCardActions>
+    </VCard>
+  </VDialog>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop, Watch } from 'nuxt-property-decorator'
+import { mdiClose } from '@mdi/js'
+import SBtn from '../components/SBtn.vue'
+
+@Component({
+  components: {
+    SBtn
+  },
+  inheritAttrs: false
+})
+export default class SDialog extends Vue {
+  icons = { mdiClose }
+
+  @Prop({ required: false, type: String }) title!: string
+  @Prop({ required: false, type: String }) text!: string
+  @Prop({ required: false, type: String }) cancelText!: string
+  @Prop({ required: false, default: 325, type: Number }) width!: number
+  @Prop({ required: true, type: Boolean }) showDialog!: boolean
+
+  innerValue: boolean | null | undefined = null
+
+  @Watch('innerValue')
+  public onInnerChange (val: this['innerValue']) {
+    this.$emit('update:show-dialog', val)
+  }
+
+  @Watch('showDialog')
+  public onOuterChange (newVal: this['showDialog']) {
+    this.innerValue = newVal
+  }
+
+  created () {
+    this.innerValue = this.showDialog
+  }
+
+  close () {
+    this.$emit('update:show-dialog', false)
+    this.$emit('close')
+  }
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ tests/unit/s-dialog-.spec.ts
@@ -0,0 +1,63 @@
+import { shallowMount, createLocalVue, Wrapper } from '@vue/test-utils'
+import { Vue } from 'vue/types/vue'
+import SDialog from '~/components/SDialog.vue'
+
+let localVue: typeof Vue
+
+describe('SDialog', () => {
+  let component: Wrapper<SDialog>
+  const title = 'Header'
+  const text = 'Lorem ipsum'
+  const cancelText = 'cancel'
+  const width = '325'
+
+  beforeEach(() => {
+    localVue = createLocalVue()
+    component = shallowMount(SDialog, {
+      localVue,
+      propsData: { showDialog: true }
+    })
+  })
+
+  it('should not have title text, or cancel button if props not passed', () => {
+    expect(component.find('vcardtitle-stub').exists()).toBeFalsy()
+    expect(component.find('vcardtext-stub').exists()).toBeFalsy()
+    expect(component.find('[data-testid=confirm-dialog-btn-cancel]').exists()).toBeFalsy()
+  })
+
+  it('should set title properly', async () => {
+    component.setProps({
+      title
+    })
+    await localVue.nextTick()
+
+    expect(component.find('vcardtitle-stub').text().includes(title)).toBeTruthy()
+  })
+
+  it('should set text properly', async () => {
+    component.setProps({
+      text
+    })
+    await localVue.nextTick()
+
+    expect(component.find('vcardtext-stub').text().includes(text)).toBeTruthy()
+  })
+
+  it('should set cancel text properly', async () => {
+    component.setProps({
+      cancelText
+    })
+    await localVue.nextTick()
+
+    expect(component.text().includes(cancelText)).toBeTruthy()
+  })
+
+  it('should pass width as prop for VDialog', async () => {
+    component.setProps({
+      width
+    })
+    await localVue.nextTick()
+
+    expect(component.find('vdialog-stub').attributes().width).toEqual(width)
+  })
+})
